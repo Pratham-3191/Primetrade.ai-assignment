a327528c1a441162b57f55b0291377dbb2186003
Pratham Chaudhari <prathamchaudhari124@gmail.com>
2025-12-31 02:39:15 +0530

    prod-err-fix3

--- api/index.js
+++ api/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
-const authRoutes = require("./routes/auth.routes");
-const taskRoutes = require("./routes/task.routes");
+const authRoutes = require("../../routes/auth.routes");
+const taskRoutes = require("../../routes/task.routes");
 
 dotenv.config();
 
 const app = express();
+
 const MONGO_URL = process.env.MONGO_URL;
 const CLIENT_URL = process.env.CLIENT_URL || "https://primetrade-assignment-flame.vercel.app";
 
@@ -20,13 +22,8 @@ app.use(cors({
   credentials: true
 }));
 
-// Preflight OPTIONS request handler
-app.options('*', cors({
-  origin: CLIENT_URL,
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-  credentials: true
-}));
+// Preflight OPTIONS requests
+app.options('*', cors());
 
 // Body parsers
 app.use(express.json());
@@ -37,30 +34,20 @@ app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 
 // Root route
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+app.get("/", (req, res) => res.send("API is running"));
 
 // Global error handler
 app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-    });
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
 });
 
 // Connect to MongoDB safely
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URL);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error);
-    // Don't crash Vercel serverless function
-  }
-};
+mongoose.connect(MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection failed:", err));
 
-connectDB();
-
-// Export app for Vercel serverless deployment
-module.exports = app;
+// Wrap Express app for Vercel serverless
+module.exports = serverless(app);
